5c85cb63a346dc057ebe8082a9be20d853ebd523
Shabab Haider Siddique <shabab.h.siddique@gmail.com>
2025-01-25 14:05:36 +0600

    added local source of verb for fallback

--- lib/helper/hive.dart
+++ lib/helper/hive.dart
@@ -5,6 +5,8 @@ import 'package:baab_practice/model/ArabicVerb.dart';
 import 'package:hive/hive.dart';
 import 'package:csv/csv.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
+
 
 abstract class VerbAppDatabase {
   static Box? box;
@@ -13,7 +15,8 @@ abstract class VerbAppDatabase {
   static const String verbListIndex = "verbs";
   static const String incorrectListIndex = "incorrect";
   static const String sourceCSVURL =
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSJoUk2VttAgxByuYVMPDNPc1I8YdpgEYOqql3xqFeJ7RxI1pLkaNrkc2pAi721c1a7bnNIxyfl56g2/pub?gid=728929932&single=true&output=csv';
+      'https://docs.google.com/aspreadsheets/d/e/2PACX-1vSJoUk2VttAgxByuYVMPDNPc1I8YdpgEYOqql3xqFeJ7RxI1pLkaNrkc2pAi721c1a7bnNIxyfl56g2/pub?gid=728929932&single=true&output=csv';
+  static const String localCSVPath = 'assets/verbs_all.csv';
 
   static Future<Box?> initHive() async {
     box = await Hive.openBox(mainVerbBox);
@@ -31,7 +34,6 @@ abstract class VerbAppDatabase {
     List<ArabicVerb> verbList = [];
 
     for (ArabicVerb verb in box!.values) {
-      print("Maadi: ${verb.maadi}, Bengali: ${verb.bengaliMeaning}");
       verbList.add(verb);
     }
     return verbList;
@@ -45,7 +47,6 @@ abstract class VerbAppDatabase {
 
     for( var baab in selectedBaabs){
       for (ArabicVerb verb in box!.values.where((verb) => verb.baab == baab)) {
-        print("Maadi: ${verb.maadi}, Bengali: ${verb.bengaliMeaning}");
         selectedVerbs.add(verb);
       }
     }
@@ -54,62 +55,66 @@ abstract class VerbAppDatabase {
   }
 
   static Future<bool> fillVerbsFromSource() async {
-    // Fetch CSV file from the link
+    bool success = await _fetchAndStoreFromNetwork();
+    if (!success ) {
+      print('Network failed, loading from local asset...');
+      success = await _fetchAndStoreFromAssets();
+    }
+    return success;
+  }
+
+  // Fetch from Network
+  static Future<bool> _fetchAndStoreFromNetwork() async {
     try {
-      // Fetch CSV file from the link
       final response = await http.get(Uri.parse(sourceCSVURL));
-
       if (response.statusCode == 200) {
-        box ??= Hive.box<ArabicVerb>(mainVerbBox);
-        final csvData = utf8.decode(response.bodyBytes);
-
-        // Parse CSV data
-        List<List<dynamic>> rows = const CsvToListConverter().convert(csvData);
-
-        // Skip header row and process the data
-        for (int i = 1; i < rows.length; i++) {
-          final row = rows[i];
-
-          ArabicVerb verb = ArabicVerb(
-            maadi: row[0].toString(),
-            mudari: row[1].toString(),
-            masdar: row[2].toString(),
-            bengaliMeaning: row[3].toString(),
-            baab: row[4].toString(),
-          );
-          var existingVerb = box!.get(verb.maadi);
-
-          if (existingVerb != null) {
-            // Compare properties to check if they are the same
-            bool isIdentical = existingVerb.mudari == verb.mudari &&
-                existingVerb.masdar == verb.masdar &&
-                existingVerb.bengaliMeaning == verb.bengaliMeaning &&
-                existingVerb.baab == verb.baab;
-
-            if (!isIdentical) {
-              // Remove existing and update with new one
-              await box!.delete(verb.maadi);
-              await box!.put(verb.maadi, verb);
-              print('Updated existing verb: ${verb.maadi}');
-            } else {
-              print('Verb already exists with identical data: ${verb.maadi}');
-            }
-          } else {
-            // Add new verb if it doesn't exist
-            await box!.put(verb.maadi, verb);
-            print('Added new verb: ${verb.maadi}');
-          }
-        }
-
-        print('CSV processing completed.');
-        return true;
+        return _processCSVData(utf8.decode(response.bodyBytes));
       } else {
-        print('Failed to load CSV: ${response.statusCode}');
+        print('Failed to load CSV from network. Status Code: ${response.statusCode}');
       }
     } catch (e) {
-      print('Error fetching or processing CSV: $e');
+      print('Error fetching CSV from network: $e');
     }
     return false;
+  }
+
+  // Fallback: Read CSV from assets
+  static Future<bool> _fetchAndStoreFromAssets() async {
+    try {
+      final csvData = await rootBundle.loadString(localCSVPath);
+      return _processCSVData(csvData);
+    } catch (e) {
+      print('Error loading CSV from assets: $e');
+    }
+    return false;
+  }
+
+  // Common function to process CSV data and store in Hive
+  static Future<bool> _processCSVData(String csvData) async {
+    box ??= Hive.box<ArabicVerb>(mainVerbBox);
+
+    List<List<dynamic>> rows = const CsvToListConverter().convert(csvData);
+
+    for (int i = 1; i < rows.length; i++) {
+      final row = rows[i];
+
+      ArabicVerb verb = ArabicVerb(
+        maadi: row[0].toString(),
+        mudari: row[1].toString(),
+        masdar: row[2].toString(),
+        bengaliMeaning: row[3].toString(),
+        baab: row[4].toString(),
+      );
+
+      var existingVerb = box!.get(verb.maadi);
+
+      //only insert verb if not exist when reading local source, no need to update.
+      if (existingVerb == null) {
+        await box!.put(verb.maadi, verb);
+      }
+    }
+
+    return true;
   }
 
 
@@ -145,7 +150,6 @@ abstract class VerbAppDatabase {
     List<ArabicVerb> failWords = [];
 
     for (ArabicVerb verb in box!.values.where((verb) => verb.failCounter > 0)) {
-      print("Maadi: ${verb.maadi}, Bengali: ${verb.bengaliMeaning}");
       failWords.add(verb);
     }
 
